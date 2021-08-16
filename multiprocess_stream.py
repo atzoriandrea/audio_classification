e4eb50d86499eb5e69f74974118cfc13cd516027
Andrea Atzori <atzoriandrea@outlook.it>
2021-08-16 19:47:30 +0200

    No more crashes with processes

--- multiprocess_stream.py
+++ multiprocess_stream.py
@@ -4,60 +4,71 @@ import wave
 from tensorflow import keras
 import numpy as np
 from multiprocessing import Process, Queue, Array
+import multiprocessing
+
 from queue import Queue
 
-model = keras.models.load_model("audio_model_bidirectional_LSTM_overlap.h5")
-
-dataset = np.load("dataset_overlap.npy")
-std_dev = np.std(dataset)
-mean = np.mean(dataset)
-maximum = np.max(dataset)
-min = np.min(dataset)
-scale = max(maximum, abs(min))
 
 
-def normalize(array, std_dev, mean, scale):
-    array = array / scale
-    return array - mean / std_dev
 
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-p = pyaudio.PyAudio()
-SPEAKERS = p.get_default_output_device_info()["hostApi"]  # The modified part
 
-p = pyaudio.PyAudio()
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                input_host_api_specific_stream_info=SPEAKERS,
-                frames_per_buffer=CHUNK)
+
+
+
 
 
 def audio_listener(frames_list, other=None):
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    p = pyaudio.PyAudio()
+    SPEAKERS = p.get_default_output_device_info()["hostApi"]  # The modified part
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    input_host_api_specific_stream_info=SPEAKERS,
+                    frames_per_buffer=CHUNK)
     while True:
         data = stream.read(CHUNK, exception_on_overflow=False)
-        frames_list.append(data)
+        frames_list.put(data)
 
 
-def audio_normalizer(frames_list, batch_queue):
+def audio_normalizer(frames_list: multiprocessing.Queue, batch_queue: multiprocessing.Queue):
+    def normalize(array, std_dev, mean, scale):
+        array = array / scale
+        return array - mean / std_dev
+
+    dataset = np.load("dataset_overlap.npy")
+    std_dev = np.std(dataset)
+    mean = np.mean(dataset)
+    maximum = np.max(dataset)
+    min = np.min(dataset)
+    scale = max(maximum, abs(min))
+
     add = False
+    CHUNK = 1024
     filled = 0
     window = np.zeros((1, 32, 16516))
     batch = 128
     batch_matrix = np.zeros((batch, 32, 16516))
     last_added = 0
+    cnk = []
     while True:
-        l = len(frames_list)
+        l = frames_list.qsize()
         if CHUNK * l >= 16384:
             s_arr = np.zeros((1, 1, 16516), dtype=np.int16)
-            cnk = frames_list[:16]
-            s_arr[0, 0, :16384] = np.frombuffer(b''.join(cnk), dtype=np.int16)
-            del frames_list[:8]
-            add = True
+            for _ in range(8):
+                cnk.append(frames_list.get())
+            if len(cnk) > 8:
+                s_arr[0, 0, :16384] = np.frombuffer(b''.join(cnk), dtype=np.int16)
+                #del frames_list[:8]
+                add = True
+                del cnk[:8]
         if filled <= 31 and add:
             window[0, filled, :] = normalize(s_arr[0, 0, :], std_dev, mean, scale)
             filled += 1
@@ -73,18 +84,19 @@ def audio_normalizer(frames_list, batch_queue):
 
 def audio_predictor(batch_queue, other=None):
     import sys
+    model = keras.models.load_model("audio_model_bidirectional_LSTM_overlap.h5")
     while True:
         q_s = batch_queue.qsize()
         if q_s > 0:
-            batch = batch_queue.get_nowait()
+            batch = batch_queue.get()
             print(batch.shape)
             sys.stdout.write(str(np.max(np.argmax(model.predict(batch), axis=-1))))
             del batch
 
 
 if __name__ == "__main__":
-    frames = []
-    audio_queue = Queue()
+    frames = multiprocessing.Queue()
+    audio_queue = multiprocessing.Queue()
 
     listener = Process(target=audio_listener, args=(frames, None))
     normalizer = Process(target=audio_normalizer, args=(frames, audio_queue))
